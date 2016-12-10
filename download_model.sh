6f567495dbb7fb3d76d7988fb7ea4676ca29169e
Keja Rowe <keja.rowe@gmail.com>
2016-12-10 00:49:36 -0500

    updated ip address of aws instance

--- download_model.sh
+++ download_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp -i ~/sdc_nano/udacity_sdc_nano_aws_key.pem ubuntu@54.153.104.166:~/behavioral_cloning/scripts/model.json .
-scp -i ~/sdc_nano/udacity_sdc_nano_aws_key.pem ubuntu@54.153.104.166:~/behavioral_cloning/scripts/model.h5 .
+scp -i ~/sdc_nano/udacity_sdc_nano_aws_key.pem ubuntu@54.67.79.18:~/behavioral_cloning/scripts/model.json .
+scp -i ~/sdc_nano/udacity_sdc_nano_aws_key.pem ubuntu@54.67.79.18:~/behavioral_cloning/scripts/model.h5 .
