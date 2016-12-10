4b92b8aaad5a54524f266e7382bf6dc3370937bb
Keja Rowe <keja.rowe@gmail.com>
2016-12-10 00:52:31 -0500

    modified to run from within data directory

--- fix_img_paths.sh
+++ fix_img_paths.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find ../data_test -name *.csv -exec sed -i -e 's/\/Users\/krowe\/sdc_nano\/behavioral_cloning/\/home\/ubuntu\/behavioral_cloning/g' {} \;
+find . -name *.csv -exec sed -i -e 's/\/Users\/krowe\/sdc_nano\/behavioral_cloning/\/home\/ubuntu\/behavioral_cloning/g' {} \;
